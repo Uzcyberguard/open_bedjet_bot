e14645611dcca362d0d3e736b1df24392100fb4f
Uzcyberguard <hasannormamatov696@gmail.com>
2026-08-26 14:04:47 +0500

    Add config

---
+++ config.py
@@ -0,0 +1,5 @@
+import os
+
+BOT_TOKEN = os.getenv("8343092702:AAFgeLnJOawRRammHmRU8nNd8bln3LlYcGY")
+OPEN_BUDGET_LINK = os.getenv("https://openbudget.uz/boards/initiatives/initiative/55/b04f18ad-58c4-4e7d-9f69-5ed3048ff9f5")
+ADMIN_GROUP_ID = int(os.getenv(-1003085360205))
