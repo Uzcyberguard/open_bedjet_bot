9c44ec892236b6644fb24102db828eb89610f6c3
Hasan Normamatov <hasannormamatov696@gmail.com>
2026-08-26 14:15:08 +0500

    Refactor config.py to use environment variable names
    Updated environment variable references for configuration.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-BOT_TOKEN = os.getenv("8343092702:AAFgeLnJOawRRammHmRU8nNd8bln3LlYcGY")
-OPEN_BUDGET_LINK = os.getenv("https://openbudget.uz/boards/initiatives/initiative/55/b04f18ad-58c4-4e7d-9f69-5ed3048ff9f5")
-ADMIN_GROUP_ID = int(os.getenv(-1003085360205))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OPEN_BUDGET_LINK = os.getenv("OPEN_BUDGET_LINK")
+ADMIN_GROUP_ID = int(os.getenv("ADMIN_GROUP_ID"))
 
